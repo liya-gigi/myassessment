f4f041e33b2693a7e6cd96fd42df4a7b9e773556
Liya Gigi <liyagigi@gmail.com>
2021-12-08 11:47:10 +0530

    made changes in alignment

--- test/pageobjects/keypress.pageobj.js
+++ test/pageobjects/keypress.pageobj.js
@@ -5,7 +5,7 @@ class Keypress {
         await browser.url("https://the-internet.herokuapp.com/key_presses");
     }
     async enterTheInput(inputValue) {
-        const inputKey = await $('#target')
+        const inputKey = await $('#target');
         await inputKey.click();
         await browser.keys(inputValue);
 
@@ -20,13 +20,13 @@ class Keypress {
         const result = await $('#result');
         await result.waitForDisplayed();
         const textOfResult = await result.getText();
-        expectchai(textOfResult).to.equal("You entered: BACK_SPACE")
+        expectchai(textOfResult).to.equal("You entered: BACK_SPACE");
     }
     async displayResultOfShiftkey() {
         const result = await $('#result');
         await result.waitForDisplayed();
         const textOfResult = await result.getText();
-        expectchai(textOfResult).to.equal("You entered: SHIFT")
+        expectchai(textOfResult).to.equal("You entered: SHIFT");
     }
     async displayResultOfSLetter() {
         const result = await $('#result');
@@ -38,7 +38,7 @@ class Keypress {
         const result = await $('#result');
         await result.waitForDisplayed();
         const textOfResult = await result.getText();
-        expectchai(textOfResult).to.equal("You entered: F1")
+        expectchai(textOfResult).to.equal("You entered: F1");
     }
 }
 module.exports = new Keypress();
