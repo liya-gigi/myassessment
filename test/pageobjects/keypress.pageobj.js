c3dca3e1d0d6f2385e2c096384d77faf5f11da03
Liya Gigi <liyagigi@gmail.com>
2021-12-08 11:43:05 +0530

    made changes

--- test/pageobjects/keypress.pageobj.js
+++ test/pageobjects/keypress.pageobj.js
@@ -13,31 +13,31 @@ class Keypress {
     async displayResultOfTab() {
         const result = await $('#result');
         await result.waitForDisplayed();
-        const textOfResult=await result.getText();
+        const textOfResult = await result.getText();
         expectchai(textOfResult).to.equal("You entered: TAB");
     }
     async displayResultOfBackspacekey() {
         const result = await $('#result');
         await result.waitForDisplayed();
-        const textOfResult=await result.getText();
+        const textOfResult = await result.getText();
         expectchai(textOfResult).to.equal("You entered: BACK_SPACE")
     }
     async displayResultOfShiftkey() {
         const result = await $('#result');
         await result.waitForDisplayed();
-        const textOfResult=await result.getText();
+        const textOfResult = await result.getText();
         expectchai(textOfResult).to.equal("You entered: SHIFT")
     }
     async displayResultOfSLetter() {
         const result = await $('#result');
         await result.waitForDisplayed();
-        const textOfResult=await result.getText();
+        const textOfResult = await result.getText();
         expectchai(textOfResult).to.equal("You entered: S")
     }
     async displayResultOfF1Key() {
         const result = await $('#result');
         await result.waitForDisplayed();
-        const textOfResult=await result.getText();
+        const textOfResult = await result.getText();
         expectchai(textOfResult).to.equal("You entered: F1")
     }
 }
--- test/specs/keypress.js
+++ test/specs/keypress.js
@@ -1,39 +1,39 @@
-const expectchai=require('chai').expect
-const Keypress=require('../pageobjects/keypress.pageobj')  
+const expectchai = require('chai').expect
+const Keypress = require('../pageobjects/keypress.pageobj')
 
 describe('key press', () => {
-    it('check for Tab key', async() => {
-       await Keypress.urlOfWebsite(); 
-       await Keypress.enterTheInput("Tab")
-       await Keypress.displayResultOfTab();
-   
-        
+    it('check for Tab key', async () => {
+        await Keypress.urlOfWebsite();
+        await Keypress.enterTheInput("Tab")
+        await Keypress.displayResultOfTab();
+
+
     });
-    it('check for Backspace key', async() => {
-        await Keypress.urlOfWebsite(); 
-       await Keypress.enterTheInput("Backspace")
-       await Keypress.displayResultOfBackspacekey();
-       
-        
+    it('check for Backspace key', async () => {
+        await Keypress.urlOfWebsite();
+        await Keypress.enterTheInput("Backspace")
+        await Keypress.displayResultOfBackspacekey();
+
+
     });
-    it('check for Shift key', async() => {
-        await Keypress.urlOfWebsite(); 
+    it('check for Shift key', async () => {
+        await Keypress.urlOfWebsite();
         await Keypress.enterTheInput("Shift")
-        await Keypress.displayResultOfShiftkey(); 
-        
+        await Keypress.displayResultOfShiftkey();
+
     });
-    it('check for s key', async() => {
-        await Keypress.urlOfWebsite(); 
+    it('check for s key', async () => {
+        await Keypress.urlOfWebsite();
         await Keypress.enterTheInput("S")
-        await Keypress.displayResultOfSLetter(); 
-        
+        await Keypress.displayResultOfSLetter();
+
     });
-    it('check for F1 key', async() => {
-        await Keypress.urlOfWebsite(); 
+    it('check for F1 key', async () => {
+        await Keypress.urlOfWebsite();
         await Keypress.enterTheInput("F1")
         await Keypress.displayResultOfF1Key();
-        
-        
+
+
     });
 
 
